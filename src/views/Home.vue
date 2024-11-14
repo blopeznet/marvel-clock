107b1f995eb42f20c9ca2a37ce971024db90a9bf
blopez <blopez@fiveflamesmobile.com>
2024-11-14 17:33:12 +0100

    Add video marvel to fix close

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,8 +8,8 @@ const comicStore = useComicStore();
 /**
  * Load data
  */
- onMounted(async () => {
-    await comicStore.fetchMarvelComics();
+onMounted(async () => {
+  await comicStore.fetchMarvelComics();
 });
 
 /**
@@ -30,6 +30,8 @@ const navigateToSettings = () => {
 
 <template>
   <div class="home">
+
+
     <div class="attributed">Data provided by Marvel. © 2014 Marvel</div>
 
     <!-- Loading ring -->
@@ -46,18 +48,20 @@ const navigateToSettings = () => {
       <p>{{ comicStore.currentDate }}</p>
     </div>
 
+    <!-- Marvel video-->
+    <video autoplay muted loop="true">
+      <source src="/video/marvel.mp4" type="video/mp4">
+    </video>
+
     <!-- Comic Background and Foreground Images -->
     <div v-if="comicStore.comicData.length" class="comic-background">
       <!-- Background Image -->
-      <img v-for="(comic, index) in comicStore.comicData" :key="`background-${index}`" 
-        :src="comic.image" alt="Background Comic Image" 
-        class="comic-background-image" 
-        :class="{ fadeIn: comicStore.isAnimated && comicStore.currentIndex === index }" 
-      />
+      <img v-for="(comic, index) in comicStore.comicData" :key="`background-${index}`" :src="comic.image"
+        alt="Background Comic Image" class="comic-background-image"
+        :class="{ fadeIn: comicStore.isAnimated && comicStore.currentIndex === index }" />
 
       <!-- Main Comic Image in the Foreground -->
-      <div v-for="(comic, index) in comicStore.comicData" :key="index" 
-        class="comic-foreground" 
+      <div v-for="(comic, index) in comicStore.comicData" :key="index" class="comic-foreground"
         :class="{ visible: comicStore.currentIndex === index }">
         <img :src="comic.image" :alt="comic.title" class="comic-img-foreground" />
       </div>
@@ -69,16 +73,19 @@ const navigateToSettings = () => {
     </div>
 
     <!-- Control Buttons -->
-    <div class="controls">      
+    <div class="controls">
       <button class="apply-button" @click="comicStore.fetchMarvelComics" aria-label="Recargar imágenes">
-        <i class="fas fa-redo"></i> 
+        <i class="fas fa-redo"></i>
       </button>
-      <button class="apply-button"  @click="toggleFullScreen">
+      <button class="apply-button" @click="toggleFullScreen">
         <i :class="comicStore.isFullScreen ? 'fas fa-compress' : 'fas fa-expand'"></i>
       </button>
-      <button class="apply-button"  @click="navigateToSettings">
+      <button class="apply-button" @click="navigateToSettings">
         <i class="fas fa-gear"></i>
       </button>
     </div>
+
+
+
   </div>
 </template>
